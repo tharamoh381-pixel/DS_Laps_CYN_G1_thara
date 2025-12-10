28dc16928214861767ec9b411462aa7df12dc1e7
DELL <thara moh381@gmail.com>
2025-12-10 11:44:36 +0300

    lab6

--- src/SinglyLinkedList/SinglyLinkedList.java
+++ src/SinglyLinkedList/SinglyLinkedList.java
@@ -1,5 +1,7 @@
 package SinglyLinkedList;
 
+import javax.xml.soap.Node;
+
 public class SinglyLinkedList<E> {
     private Node<E> head=null;
     private Node<E> tail=null;
@@ -18,6 +20,75 @@ public class SinglyLinkedList<E> {
 
 
 
+
+   public void addList(E data){
+        Node<E> newNode= new Node(data,head);
+        head=newNode;
+        if (isEmpty())
+            tail=head;
+        size++;
+   }
+
+   public E getFirst(){
+        if(isEmpty()) return null;
+        return head.getData();
+   }
+
+  public E removeFirst(){
+      if(isEmpty()) return null;
+      E delete= head.getData();
+        head=head.getNext();
+        size--;
+        if (isEmpty())
+            tail=head;
+
+        return delete;
+  }
+public void addlast(E data){
+
+       Node<E> newNode =new Node(data,null);
+       if (isEmpty())
+           head=newNode;
+       else
+           tail.setNext(newNode);
+       tail=newNode;
+       size++;
+}
+
+    public E getLast(){
+        if(isEmpty()) return null;
+        return tail.getData();
+    }
+
+    public E removeLast() {
+        if (isEmpty()) return null;
+        E delete = tail.getData();
+        if (head == tail) {
+            head = null;
+            tail = null;
+        }
+        else {
+            Node<E> temp = head;
+            while (temp.getNext() != temp) {
+                temp = temp.getNext();
+            }
+            temp.setNext(null);
+            tail=temp;
+        }
+        size--;
+        return delete;
+      }
+
+      public void display(){
+        if (isEmpty())
+            System.out.println("List is empty");
+        Node<E> temp=head;
+        while (temp!=null){
+            System.out.println(temp.getData()+"------>");
+            temp=temp.getNext();
+        }
+          System.out.println("null\n");
+      }
 
     class Node<E>{
         private E data;
