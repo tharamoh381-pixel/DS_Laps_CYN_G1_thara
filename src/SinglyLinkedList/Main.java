fbb0ecf9f113f7f4f471c13d6c95ed00c7d2ddbd
DELL <thara moh381@gmail.com>
2025-12-10 11:44:36 +0300

    lab6

--- src/SinglyLinkedList/Main.java
+++ src/SinglyLinkedList/Main.java
@@ -1,4 +1,11 @@
 package SinglyLinkedList;
 
 public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList<Integer> list=new SinglyLinkedList<Integer>();
+        list.addList(30);
+        list.addList(20);
+
+
+    }
 }
